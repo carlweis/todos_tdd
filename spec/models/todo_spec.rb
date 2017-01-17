941229b05ab02b8690b3b76ac3b63097db9aab4c
Carl Weis <carl@givecampus.com>
2017-01-17 01:32:13 -0500

    Allow marking todos incomplete

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -25,4 +25,16 @@ RSpec.describe Todo, type: :model do
       expect(todo).to be_completed
     end
   end
+
+  describe "#mark_incomplete!" do
+    it "sets complete_at to nil" do
+      todo = Todo.create!(completed_at: Time.current)
+
+      todo.mark_incomplete!
+
+      todo.reload
+
+      expect(todo).not_to be_completed
+    end
+  end
 end
